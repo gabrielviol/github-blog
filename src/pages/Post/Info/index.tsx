f373074417e8df465e21fefa903e11bd4de1eeb4
Gabriel Oliveira <gabrielviol00@hotmail.com>
2024-01-30 16:46:28 -0300

    [WIP]

--- src/pages/Post/Info/index.tsx
+++ src/pages/Post/Info/index.tsx
@@ -10,67 +10,67 @@ import { ChatIcon } from '../../../assets/Chat';
 import { difference } from '../../../utils/date';
 
 type Info = {
-  user: {
-    login: string;
-  }
-  updatedAt: string;
-  title: string;
-  url: string;
-  comments: number;
+   user: {
+      login: string;
+   }
+   updatedAt: string;
+   title: string;
+   url: string;
+   comments: number;
 }
 interface InfoProps {
-  info: Info;
-  loading: boolean;
+   info: Info;
+   loading: boolean;
 }
 
 export function Info({ info, loading }: InfoProps) {
-  // const time = formatDistanceToNow(new Date(info.updatedAt), { addSuffix: true, locale: ptBR })
-  const timePhrase = difference(new Date(info.updatedAt));
-  return (
-    <Container>
-      {loading ?
-        <Stack>
-        <Skeleton height='20px' />
-        <Skeleton height='10px' />
-        <Skeleton height='20px' />
-        <Skeleton height='10px' />
-        <Skeleton height='20px' />
-        <Skeleton height='10px' />
-      </Stack>
-        :
-        <>
-          <header>
-            <Linkk to="/">
-              <ArrowIcon />
-              <span>voltar</span>
-            </Linkk>
+   // const time = formatDistanceToNow(new Date(info.updatedAt), { addSuffix: true, locale: ptBR })
+   const timePhrase = difference(new Date(info.updatedAt));
+   return (
+      <Container>
+         {loading ?
+            <Stack>
+               <Skeleton height='20px' />
+               <Skeleton height='10px' />
+               <Skeleton height='20px' />
+               <Skeleton height='10px' />
+               <Skeleton height='20px' />
+               <Skeleton height='10px' />
+            </Stack>
+            :
+            <>
+               <header>
+                  <Linkk to="/">
+                     <ArrowIcon />
+                     <span>voltar</span>
+                  </Linkk>
 
-            <a href={info.url} className="github">
-              <span>Ver no github</span>
-              <FontAwesomeIcon icon={faArrowUpRightFromSquare} />
-            </a>
-          </header>
-          <h1>{info.title}</h1>
+                  <a href={info.url} className="github">
+                     <span>Ver no github</span>
+                     <FontAwesomeIcon icon={faArrowUpRightFromSquare} />
+                  </a>
+               </header>
+               <h1>{info.title}</h1>
 
-          <ul>
-            <li>
-              <FontAwesomeIcon icon={faGithub} />
-              <span>{info.user?.login}</span>
-            </li>
+               <ul>
+                  <li>
+                     <FontAwesomeIcon icon={faGithub} />
+                     <span>{info.user?.login}</span>
+                  </li>
 
-            <li>
-              <CalendarIcon />
-              <span>{timePhrase}</span>
-            </li>
+                  <li>
+                     <CalendarIcon />
+                     <span>{timePhrase}</span>
+                  </li>
 
-            <li>
-              <ChatIcon />
-              <span>{info.comments} comentários</span>
-            </li>
-          </ul>
-        </>
-      }
+                  <li>
+                     <ChatIcon />
+                     <span>{info.comments} comentários</span>
+                  </li>
+               </ul>
+            </>
+         }
 
-    </Container>
-  )
+      </Container>
+   )
 }
