1dc62240b6d0c61b2c98b19cda619e93cd70d617
Gabriel Oliveira <gabrielviol00@hotmail.com>
2024-01-30 16:45:54 -0300

    [WIP]

--- src/pages/Post/Content/index.tsx
+++ src/pages/Post/Content/index.tsx
@@ -5,50 +5,50 @@ import { Skeleton, Stack } from "@chakra-ui/react";
 import remarkGfm from 'remark-gfm'
 
 interface ContentProps {
-  body: string;
-  loading: boolean;
+   body: string;
+   loading: boolean;
 }
 
 export function Content({ body, loading }: ContentProps) {
-  return (
-    <Container>
-      {loading ? <>
-        <Stack>
-          <Skeleton height='30px' />
-          <Skeleton height='20px' />
-          <Skeleton height='30px' />
-          <Skeleton height='20px' />
-          <Skeleton height='30px' />
-          <Skeleton height='20px' />
-          <Skeleton height='30px' />
-          <Skeleton height='20px' />
-          <Skeleton height='30px' />
-        </Stack>
-      </> :
-        <Markdown
-          children={body}
-          remarkPlugins={[remarkGfm]}
-          components={{
-            code({ node, inline, className, children, ...props }) {
+   return (
+      <Container>
+         {loading ? <>
+            <Stack>
+               <Skeleton height='30px' />
+               <Skeleton height='20px' />
+               <Skeleton height='30px' />
+               <Skeleton height='20px' />
+               <Skeleton height='30px' />
+               <Skeleton height='20px' />
+               <Skeleton height='30px' />
+               <Skeleton height='20px' />
+               <Skeleton height='30px' />
+            </Stack>
+         </> :
+            <Markdown
+               children={body}
+               remarkPlugins={[remarkGfm]}
+               components={{
+                  code({ node, inline, className, children, ...props }) {
 
-              const match = /language-(\w+)/.exec(className || '')
-              return !inline && match ? (
-                <SyntaxHighlighter
-                  children={String(children).replace(/\n$/, '')}
-                  style={dracula as any}
-                  language={match[1]}
-                  PreTag="div"
-                  {...props}
-                />
-              ) : (
-                <code className={className} {...props}>
-                  {children}
-                </code>
-              );
-            },
-          }}
-        />
-      }
-    </Container>
-  )
+                     const match = /language-(\w+)/.exec(className || '')
+                     return !inline && match ? (
+                        <SyntaxHighlighter
+                           children={String(children).replace(/\n$/, '')}
+                           style={dracula as any}
+                           language={match[1]}
+                           PreTag="div"
+                           {...props}
+                        />
+                     ) : (
+                        <code className={className} {...props}>
+                           {children}
+                        </code>
+                     );
+                  },
+               }}
+            />
+         }
+      </Container>
+   )
 }
