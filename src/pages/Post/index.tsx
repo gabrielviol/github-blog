55038e2ecb69ce539e088c267dc20e7fed775219
Gabriel Oliveira <gabrielviol00@hotmail.com>
2024-01-30 16:46:55 -0300

    [WIP]

--- src/pages/Post/index.tsx
+++ src/pages/Post/index.tsx
@@ -9,60 +9,60 @@ const USERNAME = "gabrielviol";
 const REPO = "github-blog";
 
 type User = {
-    login: string;
+   login: string;
 }
 
 type OriginalPost = {
-    title: string;
-    updated_at: string;
-    body: string;
-    number: number;
-    user: User;
-    html_url: string;
-    comments: number;
+   title: string;
+   updated_at: string;
+   body: string;
+   number: number;
+   user: User;
+   html_url: string;
+   comments: number;
 }
 
 interface PostProps {
-    user: User;
-    title: string;
-    updatedAt: string;
-    body: string;
-    id: number;
-    url: string;
-    comments: number;
+   user: User;
+   title: string;
+   updatedAt: string;
+   body: string;
+   id: number;
+   url: string;
+   comments: number;
 }
 
 export function Post() {
-    const { id } = useParams() as { id: string };
-    const numberId = Number(id);
+   const { id } = useParams() as { id: string };
+   const numberId = Number(id);
 
-    const { data, loading } = useFetch<OriginalPost>(
-        `/repos/${USERNAME}/${REPO}/issues/${numberId}`
-    );
-    const post = useMemo(() => {
-        if (!data) return {};
+   const { data, loading } = useFetch<OriginalPost>(
+      `/repos/${USERNAME}/${REPO}/issues/${numberId}`
+   );
+   const post = useMemo(() => {
+      if (!data) return {};
 
-        const { user, body, number, updated_at, title, html_url, comments } = data;
+      const { user, body, number, updated_at, title, html_url, comments } = data;
 
-        return {
-            user: {
-                login: user.login,
-            },
-            body,
-            id: number,
-            updatedAt: updated_at,
-            title,
-            url: html_url,
-            comments,
-        };
-    }, [data]) as PostProps;
+      return {
+         user: {
+            login: user.login,
+         },
+         body,
+         id: number,
+         updatedAt: updated_at,
+         title,
+         url: html_url,
+         comments,
+      };
+   }, [data]) as PostProps;
 
-    return (
-        <div>
-        <Container>
-            <Info info={post} loading={loading}/>
-            <Content body={post.body} loading={loading}/>
-        </Container>
-        </div>
-    )
+   return (
+      <div>
+         <Container>
+            <Info info={post} loading={loading} />
+            <Content body={post.body} loading={loading} />
+         </Container>
+      </div>
+   )
 }
